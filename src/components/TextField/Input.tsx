024fe290a1e1359b82d5598a6f3ec38f63dfc204
Bill-Fashion <66458634+Bill-Fashion@users.noreply.github.com>
2022-10-15 23:38:12 +0700

    Phuc | Add | complete login page

---
+++ src/components/TextField/Input.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Input = styled.input`
+	padding: 0.75rem 0.25rem;
+	width: 100%;
+	background: #e8e1d7;
+	border: 1px solid #97826d;
+	border-radius: 0.2rem;
+	outline: none;
+`;
+
+export default Input;
---
+++ src/components/Typography/index.tsx
@@ -0,0 +1,30 @@
+import styled from "@emotion/styled";
+
+const H1 = styled.h1`
+	font-size: 2rem;
+	font-weight: 700;
+	line-height: 1.2;
+`;
+
+const H2 = styled.h2`
+	font-weight: 600;
+	font-size: 1.875rem;
+	line-height: 1.2;
+`;
+const H3 = styled.h3`
+	font-weight: 600;
+	font-size: 1.5rem;
+	line-height: 150%;
+`;
+const H4 = styled.h4`
+	font-weight: 600;
+	font-size: 1.25rem;
+	line-height: 150%;
+`;
+const P = styled.p`
+	font-weight: 400;
+	font-size: 1rem;
+	line-height: 150%;
+`;
+
+export { H1, H2, H3, H4, P };
