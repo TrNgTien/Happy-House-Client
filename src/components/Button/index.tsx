c2913ec57e8a7fad8b38e55d9dfdcf69430ae96b
Bill-Fashion <66458634+Bill-Fashion@users.noreply.github.com>
2022-10-15 23:38:12 +0700

    Phuc | Add | complete login page

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+	color: #e8e1d7;
+	font-weight: bold;
+	background-color: #342f29;
+	border-radius: 20rem;
+	padding: 0.5rem 1.25rem;
+	border: none;
+	outline: none;
+	max-width: fit-content;
+	cursor: pointer;
+`;
